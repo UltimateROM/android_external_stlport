a4d0a9c15caf3ad48a62a4203abccd27285abab4
Shilin Victor <chrono.monochrome@gmail.com>
2017-12-12 17:23:40 +0300

    Add codina libstlport
    Change-Id: I9a03988f4be34253bb4d642e83d771dd1d066baf

--- Android.mk
+++ Android.mk
@@ -1,20 +1,3 @@
-# http://b/15193147
-# Whitelist devices that are still allowed to use stlport. This will prevent any
-# new devices from making the same mistakes.
-STLPORT_WHITELIST := \
-    deb \
-    flo \
-    flounder \
-    fugu \
-    grouper \
-    hammerhead \
-    mako \
-    manta \
-    shamu \
-    tilapia \
-
-ifneq (,$(filter $(TARGET_DEVICE),$(STLPORT_WHITELIST)))
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -29,5 +12,3 @@ LOCAL_SRC_FILES_32 := $(TARGET_DEVICE)/32/$(LOCAL_MODULE).so
 endif
 
 include $(BUILD_PREBUILT)
-
-endif
